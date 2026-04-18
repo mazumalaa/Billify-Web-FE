8084fad0f52eaf2fa7c31317fcb425741531e18e
mazumalaa <mazumalaa@gmail.com>
2026-04-18 08:48:34 +0800

    create handleClick , handleChange funtions for (title, tax, friends)

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -3,55 +3,87 @@ import { RiAddBoxLine, RiBookLine, RiBox1Line, RiBox3Line, RiCalculatorLine, RiC
 import CustomSection from "../components/customSection";
 import { FiBook, FiEdit2, FiUsers } from "react-icons/fi";
 import CustomInput from "../components/customInput";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CustomButton from "../components/CustomButton";
 import { HiDatabase, HiShoppingBag, HiUsers } from "react-icons/hi";
 import { HiEquals } from "react-icons/hi2";
 
 // My-Setup
 const HomePage = () => {
+    function getLocalData(key, defaultValue) {
+        const localData = JSON.parse(localStorage.getItem(key));
+        return localData ? localData : defaultValue
+    }
+
+    function resetInput() {
+        setInput({
+            friendsName: "",
+            productName: "",
+            productPrice: "",
+            title: "",
+            tax: ""
+        })
+    }
+
     const [input, setInput] = useState({
         friendsName: "",
-        title: "",
-        tax: "",
+        productName: "",
+        productPrice: "",
+        title: getLocalData("title", ""),
+        tax: getLocalData("tax", ""),
     })
+
+    const [friends, setFriends] = useState(() => {
+        return getLocalData("friends", []);
+    });
 
     const handleChange = (key, value) => {
         setInput(prev => ({ ...prev, [key]: value }));
     }
 
+    const handleClick = (key, value, type) => {
+        if (!input.friendsName.trim()) return;
+        setFriends(prev => ([...prev, {
+            id: Date.now(),
+            name: value,
+            products: [],
+            createdAt: new Date().toISOString().split("T")[0]
+        }]));
+        resetInput();
+    }
+
+    useEffect(() => {
+        localStorage.setItem("friends", JSON.stringify(friends));
+        localStorage.setItem("title", JSON.stringify(input.title));
+        localStorage.setItem("tax", JSON.stringify(input.tax));
+        console.log(friends);
+    }, [friends, input.tax, input.title]);
+
     return (
         <div className={`columns-1 md:columns-2`}>
             <CustomSection title={`Add Friend.`} Icon={RiUserAddLine}>
-                <div className={`flex gap-2 items-center flex-col lg:flex-row`}>
+                <form onSubmit={(e) => {
+                    e.preventDefault();
+                    handleClick("friendsName", input.friendsName, "friends")
+                }} className={`flex gap-2 items-center flex-col lg:flex-row`}>
                     <CustomInput onChange={(e) => handleChange("friendsName", e.target.value)} value={input.friendsName} label={`Friend's Name`} Icon={RiUserAddLine} />
 
                     <CustomButton customStyle={`w-full lg:w-max`}>
                         <span>Add</span>
                         <RiUserAddLine />
                     </CustomButton>
-                </div>
+                </form>
             </CustomSection>
 
             <CustomSection title={`Add Title.`} Icon={RiBookLine}>
                 <div className={`flex gap-2 items-center flex-col lg:flex-row`}>
                     <CustomInput onChange={(e) => handleChange("title", e.target.value)} value={input.title} label={`Title`} Icon={FiBook} />
-
-                    <CustomButton customStyle={`w-full lg:w-max`}>
-                        <span>Add</span>
-                        <RiBookLine />
-                    </CustomButton>
                 </div>
             </CustomSection>
 
             <CustomSection title={`Add Tax.`} Icon={RiCoinLine}>
                 <div className={`flex gap-2 items-center flex-col lg:flex-row`}>
                     <CustomInput type={`number`} onChange={(e) => handleChange("tax", e.target.value)} value={input.tax} label={`Tax`} Icon={RiCoinLine} />
-
-                    <CustomButton customStyle={`w-full lg:w-max`}>
-                        <span>Add</span>
-                        <RiCoinLine />
-                    </CustomButton>
                 </div>
             </CustomSection>
 
@@ -68,9 +100,9 @@ const HomePage = () => {
 
             <CustomSection title={`Add Product.`} Icon={RiBox1Line}>
                 <div>
-                    <CustomInput onChange={(e) => handleChange("productName", e.target.value)} value={input.tax} label={`Product Name`} Icon={FiEdit2} />
-                    <CustomInput type={`number`} onChange={(e) => handleChange("productPrice", e.target.value)} value={input.tax} label={`Product Price`} Icon={RiPriceTag2Line} />
-                         <CustomButton customStyle={``}>
+                    <CustomInput onChange={(e) => handleChange("productName", e.target.value)} value={input.productName} label={`Product Name`} Icon={FiEdit2} />
+                    <CustomInput type={`number`} onChange={(e) => handleChange("productPrice", e.target.value)} value={input.productPrice} label={`Product Price`} Icon={RiPriceTag2Line} />
+                    <CustomButton customStyle={``}>
                         <span>Add</span>
                         <RiBox3Line />
                     </CustomButton>
